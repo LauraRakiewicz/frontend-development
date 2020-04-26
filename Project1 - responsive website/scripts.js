aefce24eb28c583f898be5a0ffbd5dedb1fba07b
laura.rakiewicz <rakiewicz.laura@gmail.com>
2020-04-26 09:01:52 +0200

    project1: scrolling to the bottom

--- Project1 - responsive website/scripts.js
+++ Project1 - responsive website/scripts.js
@@ -7,3 +7,7 @@ selectElement('.open').addEventListener('click', () => {
 selectElement('.close').addEventListener('click', () => {
     selectElement('nav-list').classList.remove('active');
 });
+
+function gotoBottom(){
+    window.scrollTo(0,document.body.scrollHeight);
+}

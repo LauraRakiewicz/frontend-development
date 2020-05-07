85d0f6222f5fabdd42991e551db45e7312fcfb50
laura.rakiewicz <rakiewicz.laura@gmail.com>
2020-05-07 16:44:36 +0200

    sails js crud - adding books

--- Project3 - sails js/bookbase/api/controllers/BooksController.js
+++ Project3 - sails js/bookbase/api/controllers/BooksController.js
@@ -9,10 +9,26 @@ module.exports = {
   list:function(req,res){
     Books.find({}).exec(function(err, books){
         if(err){
-            res.send(500, {error: "Db erroe"})
+            res.send(500, {error: "Db error"})
         }
         res.view('list', {books:books})
     });
+  },
+
+  add: function(req,res){
+      res.view('add')
+  },
+
+  create:function(req,res){
+    var title = req.body.title
+    var author = req.body.author
+
+    Books.create({title:title, author:author}).exec(function(err){
+        if(err){
+            res.send(500, {error: "Db error"})
+        }
+        res.redirect('/books/list');
+    })
   }
 
 };

b0bc64ee0e723372a1c71113369e9be35cf1c41b
laura.rakiewicz <rakiewicz.laura@gmail.com>
2020-06-17 08:50:12 +0200

    html css website - nav icon for mobile menu changed

--- Project1 - responsive website/scripts.js
+++ Project1 - responsive website/scripts.js
@@ -3,8 +3,22 @@ function gotoBottom() {
 }
 
 const close = document.getElementById('close');
+close.classList.add("icon-not-show")
+
+const menu = document.getElementById('menu');
+
 const navigation = document.getElementById('navigation');
 
 close.addEventListener('click', () => {
     navigation.classList.toggle('show');
+
+    close.classList.add("icon-not-show")
+    menu.classList.remove("icon-not-show")
+});
+
+menu.addEventListener('click', () => {
+    navigation.classList.toggle('show');
+
+    menu.classList.add("icon-not-show")
+    close.classList.remove("icon-not-show")
 });
